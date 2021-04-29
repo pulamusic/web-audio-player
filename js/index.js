41cf494282e94be9bb4386346cc754529ad954de
Jim Carroll <jim@pulamusic.com>
2021-04-29 17:54:54 -0400

    commit before moving forward

--- js/index.js
+++ js/index.js
@@ -1,31 +1,55 @@
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)
 
-// Volume control
-const volume = audioCtx.createGain()
-volume.connect(audioCtx.destination)
+/*
+// audio files
+const xadeFull = url(https://drive.google.com/file/d/19b6-wrGTwuHGBhzAFoAK_sZyPrwQUjnE/view?usp=sharing)
+const xadeWoodwinds = url(https://drive.google.com/file/d/1flkiCbEsjAoGz0lBIWs0Ut1Hw9tQJECj/view?usp=sharing)
+const xadeNew = url(https://drive.google.com/file/d/1U6k-OOQl5OzOS3TZXCYPZuMRAHAhsniJ/view?usp=sharing)
+*/
 
-// create the oscillating instruments sinea, sineb, sinec
-const sinea = audioCtx.createOscillator()
-const sineb = audioCtx.createOscillator()
-const sinec = audioCtx.createOscillator()
+// Create an AudioContext (cross browser)
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
 
-// sinea instrument
-sinea.frequency.value = 440 // A4(?)
-sinea.type = "sine"
-sinea.start()
-sinea.connect(volume)
+// store references to our HTML elements
+const audioElement = document.querySelector('audio');
+const playBtn = document.querySelector('button');
+const volumeSlider = document.querySelector('.volume');
 
-// sineb instrument
-sineb.frequency.value = 523.25 // note?
-sineb.type = "sine"
-sineb.start()
-sineb.connect(volume)
+// load the audio source into our audio graph
+const audioSource = audioCtx.createMediaElementSource(audioElement);
 
-// sinec instrument
-sinec.frequency.value = 698.46 // note?
-sinec.type = "sine"
-sinec.start()
-sinec.connect(volume)
+// play/pause audio
+playBtn.addEventListener('click', function() {
+	// check if context is in suspended state (autoplay policy)
+	if (audioCtx.state === 'suspended') {
+		audioCtx.resume();
+	}
 
-// gain value of volume
-volume.gain.value=0.2
+  // if track is stopped, play it
+	if (this.getAttribute('class') === 'paused') {
+		audioElement.play();
+		this.setAttribute('class', 'playing');
+		this.textContent = 'Pause'
+	// if track is playing, stop it
+} else if (this.getAttribute('class') === 'playing') {
+		audioElement.pause();
+		this.setAttribute('class', 'paused');
+		this.textContent = 'Play';
+	}
+});
+
+// if track ends
+audioElement.addEventListener('ended', function() {
+	playBtn.setAttribute('class', 'paused');
+	playBtn.textContent = 'Play'
+});
+
+// ======== Volume ========
+const gainNode = audioCtx.createGain();
+
+volumeSlider.addEventListener('input', function() {
+	gainNode.gain.value = this.value;
+});
+
+// connect our graph
+audioSource.connect(gainNode).connect(audioCtx.destination);
